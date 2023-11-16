e73baef8db21049bf94e742972c82d33a7e02cbb
zhuzhenquan <2332778528@qq.com>
2023-11-16 15:48:29 +0800

    feat: add guard

---
+++ src/guard/roles.guard.ts
@@ -0,0 +1,17 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Roles } from './roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const roles = this.reflector.get(Roles, context.getHandler());
+    if (!roles) {
+      return true;
+    }
+
+    return true;
+  }
+}
